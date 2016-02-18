d7e8b91e52f12dbd60a5f6d151e68dce648fe6d7
Dirk Eddelbuettel <edd@debian.org>
2016-02-17 20:38:53 -0600

    adding a simple example via Rcpp

---
+++ src/example1.cpp
@@ -0,0 +1,38 @@
+// This example is based on msgpack-c/example/cpp03/simple.cpp 
+
+#include <Rcpp.h>
+
+#include <msgpack.hpp>
+
+// [[Rcpp::export]]
+bool arrayEx() {
+    msgpack::type::tuple<int, bool, std::string> src(1, true, "example");
+
+    // serialize the object into the buffer.
+    // any classes that implements write(const char*,size_t) can be a buffer.
+    std::stringstream buffer;
+    msgpack::pack(buffer, src);
+
+    // send the buffer ...
+    buffer.seekg(0);
+
+    // deserialize the buffer into msgpack::object instance.
+    std::string str(buffer.str());
+
+    msgpack::unpacked result;
+
+    msgpack::unpack(result, str.data(), str.size());
+
+    // deserialized object is valid during the msgpack::unpacked instance alive.
+    msgpack::object deserialized = result.get();
+
+    // msgpack::object supports ostream.
+    std::cout << deserialized << std::endl;
+
+    // convert msgpack::object instance into the original type.
+    // if the type is mismatched, it throws msgpack::type_error exception.
+    msgpack::type::tuple<int, bool, std::string> dst;
+    deserialized.convert(dst);
+
+    return true;
+}
